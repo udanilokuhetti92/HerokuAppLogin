caf988fcd58661f3cb136a0b45fc4155471dc38a
udani lokuhetti <udani.20232492@iit.ac.lk>
2025-06-10 12:51:55 +0530

    fix: added assertations

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,6 +1,8 @@
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,6 +14,13 @@ public class LoginTest {
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!" + Keys.ENTER);
         driver.findElement(By.className("icon-signout")).click();
+
+        // Assert success message
+        WebElement message = driver.findElement(By.className("flash"));
+        System.out.println("Login message: " + message.getText());
+        Assert.assertTrue(message.getText().contains("You logged out of the secure area!"),
+                "Login failed with correct credentials!");
+
         driver.close();
     }
 
@@ -21,6 +30,13 @@ public class LoginTest {
         driver.get("https://the-internet.herokuapp.com/login");
         driver.findElement(By.id("username")).sendKeys("tomsmith12");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!" + Keys.ENTER);
+
+        // Assert Error message
+        WebElement message = driver.findElement(By.className("flash"));
+        System.out.println("Login message: " + message.getText());
+        Assert.assertTrue(message.getText().contains("Your username is invalid!"),
+                "Login failed with incorrect username!");
+
         driver.close();
     }
 
@@ -30,6 +46,12 @@ public class LoginTest {
         driver.get("https://the-internet.herokuapp.com/login");
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SecretPassword!" + Keys.ENTER);
+
+        // Assert Error message
+        WebElement message = driver.findElement(By.className("flash"));
+        Assert.assertTrue(message.getText().contains("Your password is invalid!"),
+                "Login failed with incorrect password!");
+
         driver.close();
     }
 
@@ -39,6 +61,12 @@ public class LoginTest {
         driver.get("https://the-internet.herokuapp.com/login");
         driver.findElement(By.id("username")).sendKeys("tomsmit");
         driver.findElement(By.id("password")).sendKeys("SupersecretPassword!" + Keys.ENTER);
+
+        // Assert Error message
+        WebElement message = driver.findElement(By.className("flash"));
+        Assert.assertTrue(message.getText().contains("Your username is invalid!"),
+                "Login failed with incorrect credentials!");
+
         driver.quit();
     }
 }
